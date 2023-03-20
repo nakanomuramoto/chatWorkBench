2417617ae5aad9c478492bd2b54b7c706ec296bb
MJ <junichi.muramoto@sony.com>
2023-03-20 10:31:07 +0900

    add rate

--- trial/deepLTest1.py
+++ trial/deepLTest1.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-def translate(text, target_lang):
+def translate(key, text, target_lang):
     url = "https://api-free.deepl.com/v2/translate"
     params = {
-        "auth_key": "24d88479-0606-b7ab-197e-c69616e49975:fx",
+        "auth_key": "e8967030-1822-a6bb-a532-157a9111a3c2:fx",
         "text": text,
         "target_lang": target_lang
     }
@@ -16,8 +16,27 @@ def translate(text, target_lang):
             return translations[0].get('text', '')
     return ''
 
+def checkRate(key):
+    url = "https://api-free.deepl.com/v2/usage"
+    payload = {
+        "auth_key": key,
+    }
+    response = requests.get(url, params=payload)
+    data = response.json()
+    print(data)
+    current_usage_rate = data["character_count"] / data["character_limit"]
+
+    return current_usage_rate
+
 if __name__ == '__main__':
+
+    with open('..\\keyDeepL.txt', 'r') as f:
+        keyDeepL = f.read().strip()
+
     text = input("翻訳したいテキストを入力してください: ")
     target_lang = input("翻訳後の言語を入力してください (例: JA): ")
-    translated_text = translate(text, target_lang)
+    translated_text = translate(keyDeepL, text, target_lang)
     print(translated_text)
+
+    print(f"Current usage rate: {checkRate(keyDeepL):.2%}")
+
